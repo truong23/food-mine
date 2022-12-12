707be4285739b92a242aae84e4a2365383a3b963
truong23 <truongdv.dev@gmail.com>
2022-12-12 17:22:17 +0700

    feat: config to connect mongo database,

---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,30 @@
+import { model, Schema } from "mongoose";
+
+export interface User{
+    id: string;
+    email: string;
+    password: string;
+    name: string;
+    address: string;
+    isAdmin: boolean;
+}
+
+export const UserSchema = new Schema<User>(
+    {
+        name: { type: String ,require: true},
+        email: { type: String ,require: true, unique: true},
+        password: { type: String ,require: true},
+        address: { type: String ,require: true},
+        isAdmin: { type: Boolean ,require: true},
+    },{
+        toJSON:{
+            virtuals: true
+        },
+        toObject:{
+            virtuals: true
+        },
+        timestamps: true
+    }
+)
+
+export const UserModel = model<User>('user', UserSchema)
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,14 @@
-import { sample_foods, sample_tags } from './../../frontend/src/data';
+import dotenv from 'dotenv'
+dotenv.config();
+
+
 import express from "express";
 import cors from "cors"
-import { sample_users } from './data';
-import jwt from "jsonwebtoken"
+import foodRouter from './routers/food.router'
+import userRouter from './routers/user.router'
+import { dbConnect } from './configs/database.config';
 
+dbConnect()
 const app = express();
 app.use(express.json())
 app.use(cors({
@@ -11,55 +16,8 @@ app.use(cors({
     origin: ["http://localhost:4200"]
 }))
 
-app.get("/api/foods", (req, res) => {
-    res.send(sample_foods);
-})
-
-app.get("/api/foods/search/:searchTerm", (req, res) => {
-    const searchterm = req.params.searchTerm;
-    const foods = sample_foods
-    .filter(food => food.name.toLowerCase()
-    .includes(searchterm.toLowerCase()));
-    res.send(foods)
-})
-
-app.get("/api/foods/tags", (req, res) => {
-    res.send(sample_tags)
-})
-
-app.get("/api/foods/tag/:tagName", (req, res) => {
-    const tagName = req.params.tagName
-    const foods = sample_foods.filter(food => food.tags?.includes(tagName))
-    res.send(foods)
-})
-
-app.get("/api/foods/:foodId", (req, res) => {
-    const foodId = req.params.foodId
-    const id = parseInt(foodId)
-    const food = sample_foods.find(food => food.id == id)
-    res.send(food)
-})
-
-app.post("/api/users/login", (req, res) => {
-    const body = req.body;
-    const user = sample_users.find(user => user.email === body.email && user.password === body.password)
-    if(user){
-        res.send(generateTokenResponse(user))
-    }else{
-        res.status(404).send('User name or password is not valid')
-    }
-})
-
-const generateTokenResponse = (user:any) =>{
-    const token = jwt.sign({
-        email:user.email , isAdmin: user.isAdmin
-    },"SomeRandomText",{
-        expiresIn:"30d"
-    })
-
-    user.token = token;
-    return user
-}
+app.use("/api/foods", foodRouter)
+app.use("/api/users", userRouter)
 
 const port = 5000;
 app.listen(port, () => {
