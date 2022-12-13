ccac04235fab51502e5938e2ac1030eacb0184f2
truong23 <truongdv.dev@gmail.com>
2022-12-13 17:29:45 +0700

    feat: Checkout,loading

--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -1,8 +1,10 @@
 import { sample_users } from './../data';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
 import { Router } from 'express'
 import asyncHandler from 'express-async-handler'
-import { UserModel } from '../models/user.model';
+import { User, UserModel } from '../models/user.model';
+import { HTTP_BAD_REQUEST } from '../constants/http_status';
 const router = Router()
 
 router.get("/seed", asyncHandler(
@@ -20,13 +22,12 @@ router.get("/seed", asyncHandler(
 router.post("/login", asyncHandler(
     async (req, res) => {
         const {email, password} = req.body
-        const user = await UserModel.findOne({ email: email, password: password })
+        const user = await UserModel.findOne({ email })
         
-        if(user){
+        if(user && (await bcrypt.compare(password,user.password))){
             res.send(generateTokenResponse(user))
         }else{
-            const BAD_REQUEST = 400
-            res.status(BAD_REQUEST).send("User name or password is not valid")
+            res.status(HTTP_BAD_REQUEST).send("User name or password is not valid")
         }
     }
 ))
@@ -42,15 +43,46 @@ router.post("/login", asyncHandler(
 //     }
 // })
 
-const generateTokenResponse = (user:any) =>{
+router.post('/register', asyncHandler(
+    async (req, res) => {
+        const { name, email, password, address } = req.body
+        const user = await UserModel.findOne({email})
+        if (user) {
+            res.status(HTTP_BAD_REQUEST)
+            .send('User is already exist, please login!')
+            return
+        }
+        const encryptedPassword = await bcrypt.hash(password,10)
+
+        const newUser:User = {
+            id: '',
+            name,
+            email: email.toLowerCase(),
+            password: encryptedPassword,
+            address,
+            isAdmin: false,
+        }
+
+        const dbUser = await UserModel.create(newUser)
+        res.send(generateTokenResponse(dbUser))
+    }
+))
+
+const generateTokenResponse = (user: User) =>{
     const token = jwt.sign({
         email:user.email , isAdmin: user.isAdmin
     },"SomeRandomText",{
         expiresIn:"30d"
     })
 
-    user.token = token;
-    return user
+    return {
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        address: user.address,
+        isAdmin: user.isAdmin,
+        token: token
+      };
 }
 
 export default router
